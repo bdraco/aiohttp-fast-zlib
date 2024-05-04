6de7b4d7cc133b59f2838fbbc1d7c6cb8b2f8d76
J. Nick Koston <nick@koston.org>
2024-05-04 15:54:47 -0500

    feat: add first version

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,7 +2,8 @@ import zlib as zlib_original
 from unittest.mock import patch
 
 import aiohttp.http_websocket
-import aiohttp_zlib_fast
+
+import aiohttp_fast_zlib
 
 try:
     from isal import (
@@ -15,24 +16,24 @@ except ImportError:
 def test_enable_disable():
     """Test enable/disable."""
     assert aiohttp.http_websocket.zlib is zlib_original
-    aiohttp_zlib_fast.enable()
+    aiohttp_fast_zlib.enable()
     assert aiohttp.http_websocket.zlib is expected_zlib
-    aiohttp_zlib_fast.disable()
+    aiohttp_fast_zlib.disable()
     assert aiohttp.http_websocket.zlib is zlib_original
-    aiohttp_zlib_fast.enable()
+    aiohttp_fast_zlib.enable()
     assert aiohttp.http_websocket.zlib is expected_zlib
-    aiohttp_zlib_fast.disable()
+    aiohttp_fast_zlib.disable()
 
 
 def test_enable_disable_when_all_missing():
     """Test enable/disable."""
-    with patch.object(aiohttp_zlib_fast, "best_zlib", zlib_original):
+    with patch.object(aiohttp_fast_zlib, "best_zlib", zlib_original):
         assert aiohttp.http_websocket.zlib is zlib_original
-        aiohttp_zlib_fast.enable()
+        aiohttp_fast_zlib.enable()
         assert aiohttp.http_websocket.zlib is zlib_original
-        aiohttp_zlib_fast.disable()
+        aiohttp_fast_zlib.disable()
         assert aiohttp.http_websocket.zlib is zlib_original
-        aiohttp_zlib_fast.enable()
+        aiohttp_fast_zlib.enable()
         assert aiohttp.http_websocket.zlib is zlib_original
-        aiohttp_zlib_fast.disable()
+        aiohttp_fast_zlib.disable()
         assert aiohttp.http_websocket.zlib is zlib_original
